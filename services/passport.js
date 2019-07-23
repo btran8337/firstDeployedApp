ea73d5d63ffd959b3adf0da2a4b6a8243b63a484
btran8337 <btran8337@yahoo.com>
2019-07-22 19:56:12 -0700

    complete authentication

--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,42 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require ('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+// from mongoose model instance to id
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// from id back to mongoose model instance
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, 
-  (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profileID
+          done(null, existingUser);
+        } else {
+          // we dont have a user record with this ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
